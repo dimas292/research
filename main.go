5e0a238bb5517c46f4b20d95590b3eb8f918c136
deemisterious <dimasixc22@gmail.com>
2024-08-02 10:11:13 +0700

    tipe regis endpoint

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 )
-
+// ex func
 func handlerIndex(w http.ResponseWriter, r *http.Request){
 	message := "welcome"
 	w.Write([]byte(message))
@@ -15,16 +15,28 @@ func handlerHello(w http.ResponseWriter, r *http.Request){
 }
 
 func main(){
-	
+	// closure 
+	handlerEx := func(w http.ResponseWriter, r *http.Request){
+		messages := "hellow example"
+		w.Write([]byte(messages))
+	}
+	http.HandleFunc("/ex", handlerEx)
 	http.HandleFunc("/", handlerIndex)
 	http.HandleFunc("/index", handlerIndex)
 	http.HandleFunc("/hello", handlerHello)
 	
+	// anonym
+	http.HandleFunc("/data",func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("hello again"))
+	})
 	address := "localhost:9000"
 	fmt.Printf("server started at %s\n", address)
-	err := http.ListenAndServe(address, nil)
+	server := new(http.Server)
+	server.Addr = address
+	err := server.ListenAndServe()
 	
 	if err != nil {
 		fmt.Printf(err.Error())
 	}	
+	
 }
