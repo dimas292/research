39f9e533de5fcafb590a00210cf6564ca299d8fa
deemisterious <dimasixc22@gmail.com>
2024-08-02 11:03:45 +0700

    add template simple html

--- main.go
+++ main.go
@@ -3,49 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
+	"path"
 )
+func main(){	
 
-// ex func
-// func handlerIndex(w http.ResponseWriter, r *http.Request){
-// 	message := "welcome"
-// 	w.Write([]byte(message))
-// }
-// func handlerHello(w http.ResponseWriter, r *http.Request){
-// 	message := "hello world"
-// 	w.Write([]byte(message))
-// }
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		var filepath = path.Join("views", "index.html")
+		var tmpl, err = template.ParseFiles(filepath)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		
+		var data = map[string]interface{}{
+			"title": "learning golang web",
+			"name": "batman",
+		}
+		
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+ http.Handle("/static/", 
+        http.StripPrefix("/static/", 
+            http.FileServer(http.Dir("assets"))))
 
-func main(){
-	// closure 
-	// handlerEx := func(w http.ResponseWriter, r *http.Request){
-	// 	messages := "hellow example"
-	// 	w.Write([]byte(messages))
-	// }
-	
-	// http.HandleFunc("/ex", handlerEx)
-	// http.HandleFunc("/", handlerIndex)
-	// http.HandleFunc("/index", handlerIndex)
-	// http.HandleFunc("/hello", handlerHello)
-	
-	// // anonym
-	// http.HandleFunc("/data",func(w http.ResponseWriter, r *http.Request){
-	// 	w.Write([]byte("hello again"))
-	// })
-	
-	// address := "localhost:9000"
-	// fmt.Printf("server started at %s\n", address)
-	// server := new(http.Server)
-	// server.Addr = address
-	// err := server.ListenAndServe()
-	
-	// if err != nil {
-	// 	fmt.Printf(err.Error())
-	// }	
-	http.Handle("/static/",
-		http.StripPrefix("/static", 
-			http.FileServer(http.Dir("assets"))))
-	
-	fmt.Println("server started at localhost:9000")
-	http.ListenAndServe(":9000",nil)
-	
+    fmt.Println("server started at localhost:9000")
+    http.ListenAndServe(":9000", nil)
 }
